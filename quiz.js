33b879bd843dace7d95a298422a4fb197cc2d01c
James Moses <jamxsmosxs@gmail.com>
2024-10-06 09:17:05 +0100

    Updated quiz.js

--- quiz.js
+++ quiz.js
@@ -3,10 +3,10 @@ function checkAnswer() {
     const correctAnswer = "4";
     const userAnswer = document.querySelector('[name="quiz"]:checked').value;
     console.log(userAnswer);
-    if (userAnswer !== correctAnswer) {
-        feedback.textContent = "That's incorrect. Try again!"
-    } else {
+    if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done."
+    } else {
+        feedback.textContent = "That's incorrect. Try again!"
     }
 }
 

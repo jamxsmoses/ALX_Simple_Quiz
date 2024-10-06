97027587f0957931f017c398c4b13ab87d83bd62
James Moses <jamxsmosxs@gmail.com>
2024-10-06 09:11:24 +0100

    Initial Commit

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer() {
+    const feedback = document.getElementById("feedback");
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('[name="quiz"]').value;
+    console.log(userAnswer);
+    if (userAnswer !== correctAnswer) {
+        feedback.textContent = "That's incorrect. Try again!"
+    } else {
+        feedback.textContent = "Correct! Well done."
+    }
+}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer)

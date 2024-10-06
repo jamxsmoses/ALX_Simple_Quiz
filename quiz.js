9d0d1905fc838456ea5eca55e1558af7542f6e0b
James Moses <jamxsmosxs@gmail.com>
2024-10-06 09:16:10 +0100

    Initial Commit

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkAnswer() {
     const feedback = document.getElementById("feedback");
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('[name="quiz"]').value;
+    const userAnswer = document.querySelector('[name="quiz"]:checked').value;
     console.log(userAnswer);
     if (userAnswer !== correctAnswer) {
         feedback.textContent = "That's incorrect. Try again!"
